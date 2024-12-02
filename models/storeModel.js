d9b0cd5061d2053dfe84899cb44fb640e8860136
NikoBoolean <niko.kacic@gmail.com>
2024-12-02 18:16:12 -0300

    callback se cambian por promesa para mejor manejo errores

--- models/storeModel.js
+++ models/storeModel.js
@@ -2,28 +2,32 @@
 const db = require('../app');  // Conexión a la base de datos
 
 // Función para listar todas las tiendas
-const listarTiendas = (callback) => {
-  const query = 'SELECT * FROM tienda'; 
-  
-  db.all(query, [], (err, rows) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, rows);
-    }
+const listarTiendas = () => {
+  //Cambie el callback por una promesa, en caso la respuesta no sea inediata
+  return new Promise((resolve, reject)=>{
+    const query = 'SELECT * FROM tienda'; 
+    db.all(query, [], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+    });
   });
 };
 
 // Función para obtener una tienda específica por su ID
-const obtenerTiendaPorId = (idTienda, callback) => {
-  const query = 'SELECT * FROM tienda WHERE id = ?';
-  
-  db.get(query, [idTienda], (err, row) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, row);
-    }
+const obtenerTiendaPorId = (idTienda) => {
+  //Al igual que en listar tiendas, el callback es cambiado por una promesa
+  return new Promise((resolve, reject)=>{
+    const query = 'SELECT * FROM tienda WHERE id = ?';
+    db.get(query, [idTienda], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
   });
 };
 
