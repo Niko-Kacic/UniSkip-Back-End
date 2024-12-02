eb133ea0ab33bdf10327098980df79b743336503
NikoBoolean <niko.kacic@gmail.com>
2024-12-02 00:15:39 -0300

    Se agrego arhivos de exportación de modulos, asi como script de inicio en el package

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,18 +12,28 @@ exports.getAllUsers = (req, res) => {
   });
 };
 
-exports.createUser = (req, res) => {
-  const { rut, dv_rut, nombre, apellido } = req.body;
-  const query = 'INSERT INTO usuario (rut, dv_rut, nombre, apellido) VALUES (?, ?, ?, ?)';
-  console.log(db);  // Agrega esta línea para ver el objeto db
-  db.run(query, [rut, dv_rut, nombre, apellido], function (err) {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else {
-      res.json({ message: 'Usuario creado', userId: this.lastID });
+exports.createUser = [
+  (req, res, next) => {//Se agrega validador en el req y en caso error retorna un json con el error (se debe manejar en el front, creo)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  });
-};
+    next();
+  },
+  (req, res) => {
+    const { rut, dv_rut, nombre, apellido } = req.body;
+    const query = 'INSERT INTO usuario (rut, dv_rut, nombre, apellido) VALUES (?, ?, ?, ?)';
+    console.log(db);  // Agrega esta línea para ver el objeto db
+    db.run(query, [rut, dv_rut, nombre, apellido], function (err) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else {
+        res.json({ message: 'Usuario creado', userId: this.lastID });
+      }
+    });
+  }
+];
+
 
 exports.deleteUser = (req, res) => {
   const { rut } = req.params;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,14 @@ const userController = require('../controllers/userController');
 
 // Rutas de usuarios
 router.get('/', userController.getAllUsers);
-router.post('/', userController.createUser);
+router.post("/",
+    [//Se chequea el metodo POST que no esten vacios los campos
+    check("rut").notEmpty().withMessage("El RUT es obligatorio"),
+    check("dv_rut").notEmpty().withMessage("El dígito verificador es obligatorio"),
+    check("nombre").notEmpty().withMessage("El nombre es obligatorio"),
+    check("apellido").notEmpty().withMessage("El apellido es obligatorio"),
+    ],
+  userController.createUser);
 router.delete('/:rut', userController.deleteUser);
 
 module.exports = router;  // Asegúrate de exportar correctamente el router
