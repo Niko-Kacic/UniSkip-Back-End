c42bf8e82d159da7fab441990f43e4a24fc85071
NikoBoolean <niko.kacic@gmail.com>
2024-12-02 17:29:40 -0300

    Se incorporan las rutas centralizadas en el archivo app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
-const fs = require('fs');  // Para leer el archivo SQL
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs"); // Para leer el archivo SQL
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido al servidor de Uni-Skip!');
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido al servidor de Uni-Skip!");
 });
 
 // Middleware
@@ -16,52 +16,40 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Conexión a la base de datos SQLite
-const db = new sqlite3.Database('./database/uni-skip.db', (err) => {
+const db = new sqlite3.Database("./database/uni-skip.db", (err) => {
   if (err) {
-    console.error('Error al conectar a la base de datos:', err);
+    console.error("Error al conectar a la base de datos:", err);
   } else {
-    console.log('Conectado a la base de datos SQLite.');
+    console.log("Conectado a la base de datos SQLite.");
     loadDatabaseSQL();
   }
 });
 
-module.exports = db;
-
 // Cargar archivo SQL
 const loadDatabaseSQL = () => {
-  const sql = fs.readFileSync('./database/database.sql', 'utf8');
+  const sql = fs.readFileSync("./database/database.sql", "utf8");
   db.exec(sql, (err) => {
     if (err) {
-      console.error('Error al ejecutar el archivo SQL:', err);
+      console.error("Error al ejecutar el archivo SQL:", err);
     } else {
-      console.log('Archivo SQL ejecutado correctamente.');
+      console.log("Archivo SQL ejecutado correctamente.");
     }
   });
 };
 
-// Rutas
-const productRoutes = require('./routes/productRoutes');  // Importa el router de productos
-app.use('/products', productRoutes);
+const routes = require("./routes"); //Aqui estan las rutas centralizadas
 
-const userRoutes = require('./routes/userRoutes');  // Importa el router de usuarios
-app.use('/users', userRoutes);
-
-const orderRoutes = require('./routes/orderRoutes');  // Importa el router de pedidos
-app.use('/orders', orderRoutes);
-
-const paymentRoutes = require('./routes/paymentRoutes');  // Importa el router de pagos
-app.use('/payments', paymentRoutes);
-
-const storeRoutes = require('./routes/storeRoutes');  // Importa el router de tiendas
-app.use('/stores', storeRoutes);
-
-const cartRoutes = require('./routes/cartRoutes');  // Importar rutas del carrito
-app.use('/cart', cartRoutes);
-
-const notificationRoutes = require('./routes/notificationRoutes');  // Importar rutas de notificación
-app.use('/notification', notificationRoutes);
+//Uso de las rutas
+app.use("/products", routes.productRoutes);
+app.use("/users", routes.userRoutes);
+app.use("/orders", routes.orderRoutes);
+app.use("/payments", routes.paymentRoutes);
+app.use("/stores", routes.storeRoutes);
+app.use("/cart", routes.cartRoutes);
+app.use("/notifications", routes.notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
 
+module.exports = db; //Movi el export al final por si acaso!
